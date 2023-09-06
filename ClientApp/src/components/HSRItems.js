b78ff31dda1c8a27a7c95d30bf7a935e6181b8b5
vathai <campfiraga@gmail.com>
2023-09-06 08:00:47 -0700

    added rankitems component with useState and useEffect hook

---
+++ ClientApp/src/components/HSRItems.js
@@ -0,0 +1,20 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const RankItems = () => {
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`item/${dataType}`).then((results) => {
+            return results.json();
+        }).then(data => { setItems(data); })
+    }, [])
+
+
+    return (
+        <main>
+            (items != null) ? <div>
+            </div>
+        </main>
+        )
+}
